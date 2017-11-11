f6318db6fa5fb9a77c80ddfc88a0e154b823b781
Acromyon-ПК\Acromyon <doc.domrachev@mail.ru>
2017-11-11 12:48:41 +0300

    1.5 - temporal disable adaptive features(!), rewrite JS-code with eslint.

--- handmade/js/main.js
+++ handmade/js/main.js
@@ -1,53 +1,54 @@
-"use strict";
+'use strict';
 
 /* Зёздный рейтинг */
 
-let scoreStars = document.querySelectorAll('[class*=reviews__score-star_]');
+const scoreStars = document.querySelectorAll('[class*=reviews__score-star_]');
 
-for (let i = 0; i < scoreStars.length; i++){
-	let scoreStar = scoreStars[i];
-		scoreStar.addEventListener('mouseover', function(e){
-			if (this.previousElementSibling != null){
-				for (let a = this; a.previousElementSibling != null; a = a.previousElementSibling){
-					a.previousElementSibling.classList.add("reviews__score-star_sub-hover");
-					console.log(a);
+for (let i = 0; i < scoreStars.length; i++) {
+	const scoreStar = scoreStars[i];
+		scoreStar.addEventListener('mouseover', function () {
+			if (this.previousElementSibling != null) {
+				for (let a = this; a.previousElementSibling != null; a = a.previousElementSibling) {
+					a.previousElementSibling.classList.add('reviews__score-star_sub-hover');
 				}
-		}});
-		scoreStar.addEventListener('mouseout', function(e){
-			if (this.previousElementSibling != null){
-				let subHovers = document.querySelectorAll('.reviews__score-star_sub-hover');
-				for (let j = 0; j < subHovers.length; j++){
-					let subHover = subHovers[j];
-					subHover.classList.remove("reviews__score-star_sub-hover");
+		}
+	});
+		scoreStar.addEventListener('mouseout', function () {
+			if (this.previousElementSibling != null) {
+				const subHovers = document.querySelectorAll('.reviews__score-star_sub-hover');
+				for (let j = 0; j < subHovers.length; j++) {
+					const subHover = subHovers[j];
+					subHover.classList.remove('reviews__score-star_sub-hover');
 				}
-		}});
-		scoreStar.addEventListener('click', function(e){
-				let actives = this.parentElement.querySelectorAll('.reviews__score-star_active');
-				for (let k = 0; k < actives.length; k++){
-					let active = actives[k];
-					active.classList.remove("reviews__score-star_active");
+		}
+	});
+		scoreStar.addEventListener('click', function () {
+				const actives = this.parentElement.querySelectorAll('.reviews__score-star_active');
+				for (let k = 0; k < actives.length; k++) {
+					const active = actives[k];
+					active.classList.remove('reviews__score-star_active');
 				}
-				this.classList.add("reviews__score-star_active");
-				let subHovers = document.querySelectorAll('.reviews__score-star_sub-hover');
-				for (let j = 0; j < subHovers.length; j++){
-					let subHover = subHovers[j];
-					subHover.classList.add("reviews__score-star_active");
+				this.classList.add('reviews__score-star_active');
+				const subHovers = document.querySelectorAll('.reviews__score-star_sub-hover');
+				for (let j = 0; j < subHovers.length; j++) {
+					const subHover = subHovers[j];
+					subHover.classList.add('reviews__score-star_active');
 				}
 		});
 }
 
 /* Сортировка отзывов */
 
-let sortToggle = document.getElementById('sort-toggle').children;
+const sortToggle = document.getElementById('sort-toggle').children;
 
-for (let i = 0; i < sortToggle.length; i++){
-	let sortButton = sortToggle[i];
-	sortButton.addEventListener('click', function(){
-		if(this.classList.contains("reviews__sort_selected") == false){
-			this.classList.add("reviews__sort_selected");
-			if (this.nextElementSibling != null){
-				this.nextElementSibling.classList.remove("reviews__sort_selected");
-			} else this.previousElementSibling.classList.remove("reviews__sort_selected");
+for (let i = 0; i < sortToggle.length; i++) {
+	const sortButton = sortToggle[i];
+	sortButton.addEventListener('click', function () {
+		if (this.classList.contains('reviews__sort_selected') === false) {
+			this.classList.add('reviews__sort_selected');
+			if (this.nextElementSibling != null) {
+				this.nextElementSibling.classList.remove('reviews__sort_selected');
+			} else this.previousElementSibling.classList.remove('reviews__sort_selected');
 		}
 	});
 }
