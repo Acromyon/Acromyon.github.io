3960e9b897856204d6a385fd9241eaaf0b39889c
Acromyon-ПК\Acromyon <doc.domrachev@mail.ru>
2018-08-24 00:27:10 +0300

    3.2 - fix height align of blocks (unique-tech)

--- unique-tech/js/common-es6.js
+++ unique-tech/js/common-es6.js
@@ -3,24 +3,30 @@ const doc = document;
 // Выравнивание бллоков по высоте
 
 let alignItems = doc.getElementsByClassName('height-align');
+
 getAlignItems(alignItems);
 
+function clearStyle() {
+	for (let i = 0; i < alignItems.length; i++) {
+		alignItems[i].removeAttribute('style');
+	}
+	getAlignItems(alignItems);
+}
+
 function getAlignItems(items) {
-	if (document.documentElement.clientWidth > 991) {
-		let maxItemlHeight = 0;
+	let maxItemlHeight = 0;
 
-		for (let i = 0; i < items.length; i++) {
-			if (maxItemlHeight < items[i].clientHeight) {
-				maxItemlHeight = items[i].clientHeight;
-			}
-		}
-
-		maxItemlHeight += 'px';
-
-		for (let i = 0; i < items.length; i++) {
-			items[i].style.height = maxItemlHeight;
+	for (let i = 0; i < items.length; i++) {
+		if (maxItemlHeight < items[i].clientHeight) {
+			maxItemlHeight = items[i].clientHeight;
 		}
 	}
+	maxItemlHeight += 'px';
+
+	for (let i = 0; i < items.length; i++) {
+		items[i].style.height = maxItemlHeight;
+	}
+	window.addEventListener('resize', clearStyle);
 }
 
 // Запуска анимации самолётиков
@@ -30,7 +36,7 @@ let planes = doc.getElementsByClassName('title-btn');
 doc.addEventListener('scroll', toStart);
 
 function toStart() {
-	let currentScroll = pageYOffset + doc.documentElement.clientHeight / 2;
+	let currentScroll = pageYOffset + (doc.documentElement.clientHeight / 2);
 
 	for (let i = 0; i < planes.length; i++) {
 		let coords = planes[i].getBoundingClientRect();
